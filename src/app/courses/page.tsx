715bb7e0ce34497c272ce29919c3dc860d7d3e6b
Knight161998 <sgate.kuldeep@gmail.com>
2025-02-16 20:05:02 +0530

    added workflow1

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -32,7 +32,7 @@ function page() {
               </CardItem>
               <CardItem translateZ="100" className="w-full mt-4">
                 <Image
-                  src={courses.image}
+                  src={./courses.image}
                   height="1000"
                   width="1000"
                   className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
